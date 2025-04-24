ef8ec5dd0727364c5a28e639a9c9195c83e7144f
shalini-prog <kshalini.it06@gmail.com>
2025-04-24 18:10:24 +0530

    first commit

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -11,7 +11,7 @@ const generateTokenAndSetCookie = (user,res) => {
     res.cookie("jwt-page", token, {
         httpOnly: true,
         sameSite: "none",
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
